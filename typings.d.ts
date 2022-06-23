92f7ca006ca5227b5029138292bc2fdbd2b5f319
Mahdi Parivash <mahdiparivash@protonmail.com>
2022-06-23 09:51:04 +0430

    Complate InfoCard

--- typings.d.ts
+++ typings.d.ts
@@ -7,3 +7,14 @@ export interface CardData {
   img: string;
   title: string;
 }
+export interface SearchResultData {
+  description: string;
+  img: string;
+  lat: number;
+  location: string;
+  long: number;
+  price: string;
+  star: number;
+  title: string;
+  total: string;
+}
