08de27b482b70ae51b5799588c7ab221e3f8f26d
Mahdi Parivash <mahdiparivash@protonmail.com>
2022-06-23 10:28:45 +0430

    create search functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,21 +5,43 @@ import {
   GlobeIcon,
   MenuIcon,
   UserCircleIcon,
+  UserIcon,
 } from "@heroicons/react/solid";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 import { DateRangePicker, RangeKeyDict } from "react-date-range";
+import { useRouter } from "next/dist/client/router";
 
-type Props = {};
+type Props = {
+  placeholder?: string;
+};
 
-export default function Header({}: Props) {
+export default function Header({ placeholder }: Props) {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState<Date>();
   const [endDate, setEndDate] = useState<Date>();
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
+  const router = useRouter();
 
   const handleChange = (rangesByKey: RangeKeyDict) => {
     setStartDate(rangesByKey.selection.startDate);
     setEndDate(rangesByKey.selection.endDate);
+  };
+
+  const resetInput = () => {
+    setSearchInput("");
+  };
+
+  const serach = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate?.toISOString(),
+        endDate: endDate?.toISOString(),
+        numberOfGuests,
+      },
+    });
   };
 
   const selectionRange = {
@@ -30,7 +52,10 @@ export default function Header({}: Props) {
 
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-3 md:py-1.5 2xl:py-5 px-5 md:px-10">
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer my-auto"
+      >
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
@@ -45,7 +70,7 @@ export default function Header({}: Props) {
           onChange={(e) => setSearchInput(e.target.value)}
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
           type="text"
-          placeholder="Start your serach"
+          placeholder={placeholder || "Start your search"}
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
@@ -66,6 +91,27 @@ export default function Header({}: Props) {
             rangeColors={["#FD5B61"]}
             onChange={handleChange}
           />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl pl-2 flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UserIcon className="h-5" />
+            <input
+              type="number"
+              onChange={(e) => setNumberOfGuests(Number(e.target.value))}
+              value={numberOfGuests}
+              min={1}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+          <div className="flex">
+            <button onClick={resetInput} className="flex-grow text-gray-500">
+              Cancel
+            </button>
+            <button onClick={serach} className="flex-grow text-red-400">
+              Search
+            </button>
+          </div>
         </div>
       )}
     </header>
